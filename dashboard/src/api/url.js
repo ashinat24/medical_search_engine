211d6a87dd7625d7afae24c03f9f711d9b0941b8
ashinat24 <153436289+ashinat24@users.noreply.github.com>
2024-06-21 18:05:08 +0530

    Update url.js

--- dashboard/src/api/url.js
+++ dashboard/src/api/url.js
@@ -1,2 +1,2 @@
 // export const BASE_URL = "http://localhost:8000";
-export const BASE_URL = "https://api.render.com/deploy/srv-cpqmn7ij1k6c73bjdsi0?key=0NIhM0it2XY";
+export const BASE_URL = "https://medical-search-engine-4.onrender.com/";
